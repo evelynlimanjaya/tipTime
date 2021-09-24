7ef1fa35809e82734b91857a9474d5010c06b2d3
evelynlimanjaya <crimson.eve@gmail.com>
2021-09-24 15:47:48 -0700

    Refactor Main Activity.

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -1,42 +1,36 @@
 package com.example.tiptime
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.coordinatorlayout.widget.CoordinatorLayout
 import com.example.tiptime.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_INDEFINITE
-import com.google.android.material.snackbar.BaseTransientBottomBar.LENGTH_LONG
 import com.google.android.material.snackbar.Snackbar
 import java.text.NumberFormat
 import kotlin.math.ceil
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        fun calculateTip(){
+        fun calculateTip() {
             val stringInTextField = binding.costOfService.text.toString()
             val cost = stringInTextField.toDoubleOrNull()
 
-            if(cost == null){
+            if (cost == null) {
                 binding.tipResult.text = ""
                 val emptyCostSnackbar = Snackbar
-                    .make(binding.root,getString(R.string.snackbar), LENGTH_INDEFINITE)
+                    .make(binding.root, getString(R.string.snackbar), LENGTH_INDEFINITE)
                     .setAction("Retry") {}
                 emptyCostSnackbar.show()
                 return
             }
 
-            val selectedId = binding.tipOptions.checkedRadioButtonId
-            val tipPercentage = when (selectedId){
+            val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
                 R.id.option_twenty_percent -> 0.20
                 R.id.option_eighteen_percent -> 0.18
                 R.id.option_fifteen_percent -> 0.15
@@ -45,15 +39,13 @@ class MainActivity : AppCompatActivity() {
 
             var tip = tipPercentage * cost
 
-            val roundUp = binding.roundUpSwitch.isChecked
-
-            if (roundUp){
+            if (binding.roundUpSwitch.isChecked) {
                 tip = ceil(tip)
             }
 
             val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
 
-            binding.tipResult.text=getString(R.string.tip_amount, formattedTip)
+            binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
         }
         binding.calculateButton.setOnClickListener { calculateTip() }
     }
